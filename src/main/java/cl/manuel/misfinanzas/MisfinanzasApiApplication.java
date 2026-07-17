22a7b78bb97891b36973bbd125cc7a137606f554
vo1dcod3 <x.fuentealba.ortiz@gmail.com>
2026-07-17 14:08:21 -0400

    chore: inicializar proyecto Spring Boot (Java 21, Web, JPA, PostgreSQL, Validation)

---
+++ src/main/java/cl/manuel/misfinanzas/MisfinanzasApiApplication.java
@@ -0,0 +1,13 @@
+package cl.manuel.misfinanzas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisfinanzasApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MisfinanzasApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cl/manuel/misfinanzas/MisfinanzasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.manuel.misfinanzas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MisfinanzasApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
